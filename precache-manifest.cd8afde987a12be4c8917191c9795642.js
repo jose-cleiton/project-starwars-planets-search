90c6198059697c80d8fe5eeb0ce251ead35ec6c7
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-05-02 20:58:27 -0300

    Initial commit

---
+++ precache-manifest.cd8afde987a12be4c8917191c9795642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3714f4dee5ca74ff819a530d16a6c03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c7c0f56e7f65d405f97",
+    "url": "./static/js/2.2e0d89b0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2e0d89b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2ca8585b1051fd28db",
+    "url": "./static/js/main.544dc024.chunk.js"
+  },
+  {
+    "revision": "09157014376cf56a2e0d",
+    "url": "./static/js/runtime-main.f33c1431.js"
+  }
+]);
